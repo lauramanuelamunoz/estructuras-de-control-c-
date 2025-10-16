12c307f75f8b6ac558504b33b25da564e5cec0d6
lauramanuelamunoz <laurammhmmh2000@gmail.com>
2025-10-16 14:45:43 -0500

    Add files via upload

---
+++ proyecto1/proyecto1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //--Ejercicio1---
+            //Console.WriteLine("Ingrese un número:");
+
+            //int numero = int.Parse(Console.ReadLine());
+
+            //if (numero % 2 == 0)
+            //{
+            //    Console.WriteLine("El numero que usted ingreso es: " + numero + " par");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("El numero que usted ingreso es: " + numero + " es impar");
+            //}
+
+            //--Ejercicio2--
+            //Console.WriteLine("Ingrese la primer nota:");
+            //float notaUno = float.Parse(Console.ReadLine());
+            //if (notaUno > 5.0 | notaUno < 0.0)
+            //{
+            //    Console.WriteLine("Ingrese una nota válida.");
+            //}
+            //else
+            //{
+
+            //}
+            //Console.WriteLine("Ingrese la segunda nota:");
+            //float notaDos = float.Parse(Console.ReadLine());
+            //if (notaDos > 5.0 | notaDos < 0.0)
+            //{
+            //    Console.WriteLine("Ingrese una nota válida.");
+            //}
+            //else
+            //{
+
+            //}
+            //Console.WriteLine("Ingrese la tercer nota:");
+            //float notaTres = float.Parse(Console.ReadLine());
+            //if (notaTres > 5.0 | notaTres < 0.0)
+            //{
+            //    Console.WriteLine("Ingrese una nota válida.");
+            //}
+            //else
+            //{
+
+            //}
+            //float notaUnoT = notaUno * 0.2F;
+            //float notaDosT = notaDos * 0.3F;
+            //float notaTresT = notaTres * 0.5F;
+
+            //float suma = notaUnoT + notaDosT + notaTresT;
+            //Console.WriteLine($"Su nota es: {suma}");
+            //if (suma >= 3.0)
+            //{
+            //    Console.WriteLine("Aprobó.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No aprobó.");
+            //}
+
+
+            //--Ejercicio3--
+            //Console.WriteLine("Ingrese el precio del producto:");
+            //float precio = float.Parse(Console.ReadLine());
+
+            //if (precio >= 100000)
+            //{
+            //    float preci1 = precio * 0.8F;
+            //    Console.WriteLine($"Tienes un descuento del 20%, el precio queda en: {preci1}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No tiene descuento, el precio es: " + precio);
+            //}
+
+
+        }
+    }
+}
+       
