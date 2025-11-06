fc6759a8960ce735e27fe5acba5f53416f7e4b7f
lauramanuelamunoz <laurammhmmh2000@gmail.com>
2025-11-06 15:16:16 -0500

    Add files via upload

---
+++ CrudNativo/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudNativo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext : DbContext options) : base(options)
+            {
+            }
+            public DbSet <Models.Producto> Productos {  get; set; }
+    
+}
